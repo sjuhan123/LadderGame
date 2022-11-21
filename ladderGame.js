472c5bb50a5b94281588db763037aaecb8583c94
한승주 <sjuhan123@gmail.com>
2022-11-22 00:17:07 +0900

    프리코스 4주차 1번째 미션 사다리게임 3번

--- ladderGame.js
+++ ladderGame.js
@@ -1,3 +1,17 @@
+// 2. 사다리 인원 입력
+// process.argv를 이용하여 사다리 게임에 참여할 인원을 입력받는다.
+// 예: node index.js 3 로 실행하면 3을 읽도록 함.
+// 만약 사다리 인원이 2명 미만이거나 8명을 초과할 경우 프로그램을 종료한다.
+
+// const process = require('process');
+
+// const args = process.argv;
+
+// if( args[2] > 2 && args[2] < 8 ){
+//     console.log(`3번 째 요소 인수 값: + ${args[2]}`);
+// }
+
+
 // 3. 사다리 출력하기
 // 사다리 높이는 4개라고 가정한다.
 // 참여 인원과 높이를 매개변수로 받아서 사다리를 출력하는 printLadder() 함수를 구현한다.
@@ -10,7 +24,49 @@ const process = require('process');
 
 const args = process.argv;
 
-if( args[2] > 2 && args[2] < 8 ){
-    console.log(`3번 째 요소 인수 값: + ${args[2]}`);
+const ladderHeight = args[2];
+const numberOfParticipants = args[3];
+
+function slashOrblank(){
+    const randomNumber = Math.random() * 10;
+    const roundNumber = Math.round(randomNumber);
+
+    if( roundNumber % 2 === 0){
+        return '-';
+    } else {
+        return ' ';
+    }
 }
+
+function printLadderIn2DArray(heigth, length){
+    const array = [];
+
+    for(let i = 0; i < heigth; i++){
+        const cells = [];
+        for(let j = 0; j < length ;j++){
+            if( j === 0 ){
+                cells.push('|');
+            } else if( j % 2 === 1){
+                cells.push(slashOrblank());
+            } else {
+                cells.push('|');
+            }
+        }
+        array.push(cells);
+    }
+    return array;
+}
+
+function printLadder(height, participants){
+    const columnLength = participants * 2 - 1;
+
+    const laderIn2DArray = printLadderIn2DArray(height, columnLength);
+
+    for( i = 0; i < laderIn2DArray.length; i++){
+       console.log(laderIn2DArray[i].join(""));
+    }
+}
+
+const answer = printLadder(ladderHeight, numberOfParticipants);
+console.log(answer);
 
