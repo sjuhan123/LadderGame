9f2811a66686ac37656d5a701fd0b281536a3acd
한승주 <sjuhan123@gmail.com>
2022-11-22 17:10:45 +0900

    2번 문제 3번 문제 이어 붙이기.

--- ladderGame.js
+++ ladderGame.js
@@ -1,31 +1,17 @@
-// 2. 사다리 인원 입력
-// process.argv를 이용하여 사다리 게임에 참여할 인원을 입력받는다.
-// 예: node index.js 3 로 실행하면 3을 읽도록 함.
-// 만약 사다리 인원이 2명 미만이거나 8명을 초과할 경우 프로그램을 종료한다.
+// 사다리 게임 출력하기
 
-// const process = require('process');
-
-// const argv = process.argv;
-
-// if( argv[2] > 2 && argv[2] < 8 ){
-//     console.log(`3번 째 요소 인수 값: + ${argv[2]}`);
-// }
-
-
-// 3. 사다리 출력하기
-// 사다리 높이는 4개라고 가정한다.
-// 참여 인원과 높이를 매개변수로 받아서 사다리를 출력하는 printLadder() 함수를 구현한다.
-// 가로 개수는 참여 인원 - 1, 세로 개수는 사다리 높이와 동일한 Int 타입 2차원 배열을 만들고
-// Math.random으로 랜덤하게 사다리 발판이 있으면 -를 출력하고, 없으면 공백을 출력한다.
-// 발판 사이에는 |로 표시한다.
-// 실행해서 콘솔에 출력되는지 확인한다.
-
+const { exit } = require('process');
 const process = require('process');
 
 const argv = process.argv;
 
 const ladderHeight = argv[2];
 const numberOfParticipants = argv[3];
+
+if( numberOfParticipants < 2 || numberOfParticipants > 8 ){
+    console.log("참여 인원 조건에 의해 프로그램 종료합니다.")
+    exit();
+}
 
 function eitherSlashOrblank(){
     const randomNumber = Math.random() * 10;
